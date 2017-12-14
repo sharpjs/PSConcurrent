ccd6338d8d333b9892f37e92df0df7e439444419
Jeff Sharp <jeff.sharp@git>
2017-12-14 09:58:03 -0800

    Clean up usings.

--- PSParallel/InvokeParallelCmdlet.cs
+++ PSParallel/InvokeParallelCmdlet.cs
@@ -16,8 +16,6 @@
 
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
 using System.Management.Automation;
 using System.Management.Automation.Host;
 using System.Management.Automation.Runspaces;
