6a605df2e54e9370945c7bab21b17975f1f1f205
Jeff Sharp <jeff.sharp@git>
2017-12-23 16:01:23 -0800

    Improve basic tests.

--- PSConcurrent.Tests/InvokeConcurrentCmdletTests.cs
+++ PSConcurrent.Tests/InvokeConcurrentCmdletTests.cs
@@ -14,7 +14,13 @@
     OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace PSConcurrent.Tests
@@ -22,18 +28,67 @@ namespace PSConcurrent.Tests
     [TestFixture]
     public class InvokeConcurrentCmdletTests
     {
-        [Test]
-        public void TestIt()
-        {
-            using (var shell = PowerShell.Create())
-            {
-                shell.AddScript(@"
-                    Import-Module PSConcurrent.dll
-                    Invoke-Concurrent { 42 }, { 42 }
-                ");
+        private InitialSessionState  InitialState;
 
-                shell.Invoke();
-            }
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            InitialState = InitialSessionState.CreateDefault();
+
+            InitialState.Variables.Add(new SessionStateVariableEntry(
+                "ErrorActionPreference", "Stop", null
+            ));
+
+            var modulePath = Path.Combine(
+                TestContext.CurrentContext.TestDirectory,
+                "PSConcurrent.psd1"
+            );
+
+            InitialState.ImportPSModule(new[] { modulePath });
+        }
+
+        [Test]
+        public void OneScriptWithOutput()
+        {
+            var output = Run(@"
+                Invoke-Concurrent { 42 }
+            ");
+
+            output.Raw.Should().HaveCount(1);
+
+            output.OfWorker(1).Should().Contain(42);
+        }
+
+        [Test]
+        public void MultiScriptWithOutput()
+        {
+            var output = Run(@"
+                Invoke-Concurrent { 42 }, { 123 }, { 31337 }
+            ");
+
+            output.Raw.Should().HaveCount(3);
+
+            output.OfWorker(1).Should().Contain(   42);
+            output.OfWorker(2).Should().Contain(  123);
+            output.OfWorker(3).Should().Contain(31337);
+        }
+
+        private Output Run(string script)
+        {
+            using (var shell = PowerShell.Create(InitialState))
+                return new Output { Raw = shell.AddScript(script).Invoke() };
+        }
+
+        private class Output
+        {
+            public ICollection<PSObject> Raw;
+
+            public IEnumerable<object> OfWorker(int id)
+                => Raw
+                    .Select(o => o.BaseObject)
+                    .OfType<WorkerOutput>()
+                    .Where(o => o.WorkerId == id)
+                    .Select(o => o.Object);
         }
     }
 }
