a36a715dc2ec2565ed9f96f43eec44ade275a751
Jeff Sharp <jeff.sharp@git>
2018-12-11 06:24:26 -0800

    Use v prefix for version tags, not release/.

--- Set-Version.ps1
+++ Set-Version.ps1
@@ -9,28 +9,13 @@
     * In Local.props, to set assembly and nupkg versions
     * As console output, to set the TeamCity build number
 
-    Version      Branch            Counter  =>  Version             FileVersion
-    ===========  ================  =======      ==================  ===========
-    1.2.3-local  (none)             (none)  =>  1.2.3-local         1.2.3.43210 (time-based)
-    1.2.3-local  stuff                 789  =>  1.2.3-stuff-b789    1.2.3.789
-    1.2.3-local  pull/456              789  =>  1.2.3-pr456-b789    1.2.3.789
-    1.2.3-local  release/1.2.3-rc      789  =>  1.2.3-rc            1.2.3.789
-    1.2.3-local  release/2.3.4-rc      789  =>  *ERROR*             *ERROR*
-
-.NOTES
-    Copyright (C) 2018 Jeffrey Sharp
-
-    Permission to use, copy, modify, and distribute this software for any
-    purpose with or without fee is hereby granted, provided that the above
-    copyright notice and this permission notice appear in all copies.
-
-    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+    Code Version  -Branch    -Counter  =>  Version             FileVersion
+    ============  =========  ========      ==================  ===========
+    1.2.3-local   (none)       (none)  =>  1.2.3-local         1.2.3.43210 (time-based)
+    1.2.3-local   stuff           789  =>  1.2.3-stuff-b789    1.2.3.789
+    1.2.3-local   456             789  =>  1.2.3-pr456-b789    1.2.3.789
+    1.2.3-local   v1.2.3-rc       789  =>  1.2.3-rc            1.2.3.789
+    1.2.3-local   v2.3.4-rc       789  =>  *ERROR*             *ERROR*
 #>
 param (
     # Name of the branch or tag.  Default: "local".
@@ -47,11 +32,18 @@ param (
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
+$PullRequestRegex = [regex] '(?nx)
+    ^
+    ( pull/ )?
+    (?<Number> [1-9][0-9]* )
+    $
+'
+
 # SemVer 1.0 + restriction that tag parts must start with alpha char
 # https://semver.org/spec/v1.0.0.html
 $VersionRegex = [regex] '(?nx)
     ^
-    ( release/ )?
+    ( release/ | v )?
     (?<VersionFull>
         (?<Version>
             ( 0 | [1-9][0-9]* ) \.
