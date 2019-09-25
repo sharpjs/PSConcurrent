6c701afde8a5b57fac052fa30501109d2613208c
Jeff Sharp <public@sharpjs.net>
2019-09-25 07:57:52 -0700

    Add array.CompactOrEmpty()

---
+++ PSConcurrent/ArrayExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace PSConcurrent
+{
+    internal static class ArrayExtensions
+    {
+        internal static T[] CompactOrEmpty<T>(this T?[]? array)
+            where T : class
+        {
+            if (array == null)
+                return new T[0];
+
+            for (var i = 0; i < array.Length; i++)
+                if (array[i] is null)
+                    return array.CompactToNew();
+
+            return array!;
+        }
+
+        private static T[] CompactToNew<T>(this T?[] array)
+            where T : class
+        {
+            var count = 0;
+
+            for (var i = 0; i < array.Length; i++)
+                if (null != array[i])
+                    count++;
+
+            var result = new T[count];
+            count = 0;
+            T? item;
+
+            for (var i = 0; i < array.Length; i++)
+                if (null != (item = array[i]))
+                    result[count++] = item;
+
+            return result;
+        }
+    }
+}
