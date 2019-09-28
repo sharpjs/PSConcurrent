0c0699a0ff06d8b2320173d009a5034fac5eeb53
Jeff Sharp <public@sharpjs.net>
2019-09-28 10:46:46 -0700

    Make it more convenient to access NUnit Randomizer.

---
+++ PSConcurrent.Tests/TestExtensions.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace PSConcurrent
+{
+    internal static class TestExtensions
+    {
+        public static Randomizer Random
+            => TestContext.CurrentContext.Random;
+    }
+}
