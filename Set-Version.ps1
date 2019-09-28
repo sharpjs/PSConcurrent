ed8ec2f262a74811cf542ea2c6d87a53e36c789d
Jeff Sharp <public@sharpjs.net>
2019-09-28 15:04:15 -0700

    Copyright

--- Set-Version.ps1
+++ Set-Version.ps1
@@ -16,6 +16,20 @@
     1.2.3-local   456             789  =>  1.2.3-pr456-b789    1.2.3.789
     1.2.3-local   v1.2.3-rc       789  =>  1.2.3-rc            1.2.3.789
     1.2.3-local   v2.3.4-rc       789  =>  *ERROR*             *ERROR*
+
+    Copyright (C) 2019 Jeffrey Sharp
+
+    Permission to use, copy, modify, and distribute this software for any
+    purpose with or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #>
 param (
     # Name of the branch or tag.  Default: "local".
